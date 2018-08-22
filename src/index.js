254c2cbd1989bd82135a584f3d2413078fcd186d
Dusan Stevanovic <dooshanstevanovic@gmail.com>
2018-08-22 18:59:21 +0200

    add packages and webpack config change

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('dusan')
+import React from 'react';
+import 'babel-polyfill';
+import { render } from 'react-dom';
+import { store } from './store';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+
+render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('app')
+);
