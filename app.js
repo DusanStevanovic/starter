a63ec70e535d8dcf73e4a8f3df36679207dc8b43
Dusan Stevanovic <dooshanstevanovic@gmail.com>
2018-08-22 15:41:48 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
+import config from './webpack.config';
+import open from 'open';
+
+/* eslint-disable no-console */
+
+const port = 9999;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './src/index.html'));
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        open(`http://localhost:${port}`);
+    }
+});
