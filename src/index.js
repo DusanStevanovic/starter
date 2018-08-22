e4389803f357a112f700b6aaf948c7e09f89c4f8
Dusan Stevanovic <dooshanstevanovic@gmail.com>
2018-08-22 19:02:49 +0200

    making index.js empty again

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import React from 'react';
-import 'babel-polyfill';
-import { render } from 'react-dom';
-import { store } from './store';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-
-render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>,
-    document.getElementById('app')
-);
+console.log('starter!!!')
